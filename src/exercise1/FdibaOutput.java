e348e241cce12aa6c6664c998e45ae4d910bd7d7
victory310 <>
2021-10-13 14:32:36 +0000

    Files updated

--- src/exercise1/FdibaOutput.java
+++ src/exercise1/FdibaOutput.java
@@ -6,7 +6,7 @@ public class FdibaOutput {
         System.out.println("FFFFF DDDDD   II BBBBB      A");
         System.out.println("F     D    D  II B    B    A A");
         System.out.println("FFFF  D     D II BBBBB    AAAAA");
-        System.out.println("FFFF  D     D II BBBBB    AAAAA");
+        System.out.println("F     D    D  II B    B  A     A");
         System.out.println("F     DDDDD   II BBBBB  A       A");
 
     }

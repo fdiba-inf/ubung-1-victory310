dd63d0495ea26d91618661e70c7f67d32403e9a6
victory310 <>
2021-10-13 14:28:55 +0000

    Files updated

---
+++ src/exercise1/Tree.java
@@ -0,0 +1,15 @@
+package exercise1;
+
+public class Tree {
+
+    public static void main(String[] args) {
+        System.out.println("     *     ");
+        System.out.println("    ***    ");
+        System.out.println("   *****   ");
+        System.out.println("  *******  ");
+        System.out.println(" ********* ");
+        System.out.println("    ***    ");
+
+    }
+
+}
